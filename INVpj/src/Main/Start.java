b154ff78bc9f4dc320765df855e9699af0dbdf0a
User <User@YD03-17>
2018-10-01 16:50:16 +0900

    111

--- INVpj/src/Main/Start.java
+++ INVpj/src/Main/Start.java
@@ -1,10 +1,17 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Start {
 
+	private int n=0;
+	private 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		asdasd sadddd ghghghgsadasdsad
+		Scanner sc = new Scanner(System.in);
+		System.out.println("메뉴를 입력하세요");
+		
 	}
 
 }
