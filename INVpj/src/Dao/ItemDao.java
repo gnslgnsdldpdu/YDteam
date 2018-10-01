66474465d6a90bca7c279885bea634d174a108d7
User <User@YD03-17>
2018-10-01 17:39:41 +0900

    111

--- INVpj/src/Dao/ItemDao.java
+++ INVpj/src/Dao/ItemDao.java
@@ -1,18 +1,22 @@
 package Dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class GoodsDao {
+import Dto.ItemDto;
+
+public class ItemDao {
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
 	private String sql;
-
-	public GoodsDao() {
+	public int r;
+	
+	public ItemDao() {
 		try {
 			String user = "hr";
 			String pw = "hr";
@@ -35,11 +39,23 @@ public class GoodsDao {
 		}return rs;
 	}
 	
-	public boolean insertItem(ItemDto dto) {
-		boolean result = false;
-		sql = "insert into "
+	public ResultSet insertItem(ItemDto dto) {
+		sql = "insert into input_t values(LPAD(Seq_input.nextval,8,'0'),:buy_num,line,i_code,i_name,quan,unitp,price,inputdate)";
 		try {
-			
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			pstmt.setString(1, dto.getB_num());
+			pstmt.setInt(2, dto.getLine());
+			pstmt.setString(2, dto.getI_code());
+			pstmt.setString(3, dto.getI_name());
+			pstmt.setInt(4, dto.getQuan());
+			pstmt.setInt(5, dto.getPrice());
+			pstmt.setDate(6, (Date) dto.getO_date());
+			pstmt.setDate(7, (Date) dto.getI_date());
+			int r = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
 		}
+		return rs;
 	}
 }
--- INVpj/src/Dto/ItemDto.java
+++ INVpj/src/Dto/ItemDto.java
@@ -1,40 +1,78 @@
 package Dto;
 
-public class ItemDto {
-	private String BuyIt;
-	private String SellIt;
-	private String BuyUsr;
-	private String SellUsr;
-	
+import java.util.Date;
 
-	public String getBuyIt() {
-		return BuyIt;
+public class ItemDto {
+	private String b_num;
+	private String s_num;
+	private String i_code;
+	private String i_name;
+	private int line;
+	private int quan;
+	private int price;
+	private Date i_date;
+	private Date o_date;
+	
+	public String getB_num() {
+		return b_num;
 	}
-	public void setBuyIt(String buyIt) {
-		BuyIt = buyIt;
+	public void setB_num(String b_num) {
+		this.b_num = b_num;
 	}
-	public String getSellIt() {
-		return SellIt;
+	public String getS_num() {
+		return s_num;
 	}
-	public void setSellIt(String sellIt) {
-		SellIt = sellIt;
+	public void setS_num(String s_num) {
+		this.s_num = s_num;
 	}
-	public String getBuyUsr() {
-		return BuyUsr;
+	public String getI_code() {
+		return i_code;
 	}
-	public void setBuyUsr(String buyUsr) {
-		BuyUsr = buyUsr;
+	public void setI_code(String i_code) {
+		this.i_code = i_code;
 	}
-	public String getSellUsr() {
-		return SellUsr;
+	public String getI_name() {
+		return i_name;
 	}
-	public void setSellUsr(String sellUsr) {
-		SellUsr = sellUsr;
+	public void setI_name(String i_name) {
+		this.i_name = i_name;
+	}
+	public int getLine() {
+		return line;
+	}
+	public void setLine(int line) {
+		this.line = line;
+	}
+	public int getQuan() {
+		return quan;
+	}
+	public void setQuan(int quan) {
+		this.quan = quan;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Date getI_date() {
+		return i_date;
+	}
+	public void setI_date(Date i_date) {
+		this.i_date = i_date;
+	}
+	public Date getO_date() {
+		return o_date;
+	}
+	public void setO_date(Date o_date) {
+		this.o_date = o_date;
 	}
 	
 	@Override
 	public String toString() {
-		return "ItemDto [BuyIt=" + BuyIt + ", SellIt=" + SellIt + ", BuyUsr=" + BuyUsr + ", SellUsr=" + SellUsr + "]";
+		return "ItemDto [b_num=" + b_num + ", s_num=" + s_num + ", i_code=" + i_code + ", i_name=" + i_name + ", quan="
+				+ quan + ", price=" + price + ", i_date=" + i_date + ", o_date=" + o_date + "]";
 	}
+	
 	
 }
