2db08f20b7ab456005d9c5e85cac4776733af940
User <User@YD03-00>
2018-10-02 09:02:33 +0900

    t

--- INVpj/src/Dto/BuySellDto.java
+++ INVpj/src/Dto/BuySellDto.java
@@ -1,41 +1,50 @@
 package Dto;
 
 public class BuySellDto {
-private String buyer;
-private String sellers;
-private String cp_name;
-private String add;
-private int ph;
-public String getBuyer() {
-	return buyer;
-}
-public void setBuyer(String buyer) {
-	this.buyer = buyer;
-}
-public String getSellers() {
-	return sellers;
-}
-public void setSellers(String sellers) {
-	this.sellers = sellers;
-}
-public String getCp_name() {
-	return cp_name;
-}
-public void setCp_name(String cp_name) {
-	this.cp_name = cp_name;
-}
-public String getAdd() {
-	return add;
-}
-public void setAdd(String add) {
-	this.add = add;
-}
-public int getPh() {
-	return ph;
-}
-public void setPh(int ph) {
-	this.ph = ph;
-}
+	private String buyer;
+	private String sellers;
+	private String cp_name;
+	private String add;
+	private int ph;
 
+	public String getBuyer() {
+		return buyer;
+	}
+
+	public void setBuyer(String buyer) {
+		this.buyer = buyer;
+	}
+
+	public String getSellers() {
+		return sellers;
+	}
+
+	public void setSellers(String sellers) {
+		this.sellers = sellers;
+	}
+
+	public String getCp_name() {
+		return cp_name;
+	}
+
+	public void setCp_name(String cp_name) {
+		this.cp_name = cp_name;
+	}
+
+	public String getAdd() {
+		return add;
+	}
+
+	public void setAdd(String add) {
+		this.add = add;
+	}
+
+	public int getPh() {
+		return ph;
+	}
+
+	public void setPh(int ph) {
+		this.ph = ph;
+	}
 
 }
